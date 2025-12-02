d37b2bc3119c55bf4cba97478188e84651a58daf
신준규 <126842353+shin1244@users.noreply.github.com>
2025-12-02 11:07:18 +0900

    코드 리팩토링

--- main.go
+++ main.go
@@ -25,80 +25,15 @@ func main() {
 	conn, _ := net.ListenUDP("udp", laddr)
 	defer conn.Close()
 
-	packetCh := make(chan Pixel)
+	drawCh := make(chan Pixel)
 	checkCh := make(chan Pixel)
 
 	img := NewImage()
 
-	go func() {
-		buf := make([]byte, 128)
-		for {
-			n, addr, err := conn.ReadFromUDP(buf)
-			if err != nil {
-				continue
-			}
-			p := parsePixel(buf[:n], addr)
-			packetCh <- p
-		}
-	}()
+	go readUDPPackets(conn, drawCh, checkCh)
+	go requestMissingPixels(conn, checkCh)
+	go drawPixels(img, drawCh)
 
-	go func() {
-		// 1차원 배열로 체크하는 게 인덱스 계산하기 더 편함
-		received := make([]bool, 65536)
-
-		var verifyTimer <-chan time.Time
-		var lastAddr *net.UDPAddr
-
-		for {
-			select {
-			case p := <-checkCh:
-				received[p.index] = true
-
-				if p.index == 65535 {
-					verifyTimer = time.Tick(1000 * time.Millisecond)
-					lastAddr = p.addr
-				}
-
-			case <-verifyTimer:
-				missingCount := 0
-
-				reqBuf := make([]byte, 2)
-
-				for i := 0; i < 65536; i++ {
-					if !received[i] {
-						binary.BigEndian.PutUint16(reqBuf, uint16(i))
-
-						if lastAddr != nil {
-							conn.WriteToUDP(reqBuf, lastAddr)
-						}
-
-						missingCount++
-
-						if missingCount%100 == 0 {
-							time.Sleep(time.Millisecond * 1)
-						}
-					}
-				}
-
-				if missingCount == 0 {
-					fmt.Println("완벽함! 모든 픽셀 수신 완료.")
-					verifyTimer = nil
-				} else {
-					fmt.Printf("총 %d개의 패킷 재요청 보냄.\n", missingCount)
-				}
-
-			}
-		}
-	}()
-
-	go func() {
-		for p := range packetCh {
-			x := int(p.index % 256)
-			y := int(p.index / 256)
-			img.pixels[y][x] = p.color
-			checkCh <- p
-		}
-	}()
 	ebiten.SetWindowSize(256, 256)
 	ebiten.SetWindowTitle("Image Viewer")
 
@@ -132,5 +67,67 @@ func (i *Image) Draw(screen *ebiten.Image) {
 		for x := range 256 {
 			screen.Set(x, y, i.pixels[y][x])
 		}
+	}
+}
+
+func readUDPPackets(conn *net.UDPConn, drawCh chan<- Pixel, checkCh chan<- Pixel) {
+	buf := make([]byte, 128)
+	for {
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			continue
+		}
+		p := parsePixel(buf[:n], addr)
+		drawCh <- p
+		checkCh <- p
+	}
+}
+
+func requestMissingPixels(conn *net.UDPConn, checkCh <-chan Pixel) {
+	received := make([]bool, 65536)
+	reqBuf := make([]byte, 2)
+
+	var ticker *time.Ticker
+	var lastAddr *net.UDPAddr
+
+	for {
+		select {
+		case p := <-checkCh:
+			received[p.index] = true
+
+			if p.index == 65535 {
+				if ticker != nil {
+					ticker.Stop()
+				}
+				ticker = time.NewTicker(1000 * time.Millisecond)
+				lastAddr = p.addr
+			}
+
+		case <-ticker.C:
+			missingCount := 0
+
+			for i := 0; i < 65536; i++ {
+				if !received[i] {
+					binary.BigEndian.PutUint16(reqBuf, uint16(i))
+					conn.WriteToUDP(reqBuf, lastAddr)
+					missingCount++
+				}
+			}
+			if missingCount == 0 {
+				fmt.Println("All pixels received!")
+				ticker.Stop()
+				ticker = nil
+			} else {
+				fmt.Printf("Requested %d missing packets.\n", missingCount)
+			}
+		}
+	}
+}
+
+func drawPixels(img *Image, drawCh <-chan Pixel) {
+	for p := range drawCh {
+		x := int(p.index % 256)
+		y := int(p.index / 256)
+		img.pixels[y][x] = p.color
 	}
 }
